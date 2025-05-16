d9ca743e7f5e486375d5dd7db5bc25ce38972316
Shubham Chaudhary <71214613+Shubhamc4@users.noreply.github.com>
2025-05-16 15:36:46 +0530

    Update BaseEnum.php

--- Traits/BaseEnum.php
+++ Traits/BaseEnum.php
@@ -1,102 +1,125 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App\Enums\Traits;
+
+use Error;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Str;
+use UnitEnum;
 use ValueError;
 
 trait BaseEnum
 {
+    public static function __callStatic(string $name, mixed $args): null|int|string
+    {
+        return self::fromName(name: $name)->value;
+    }
+
     public static function names(): array
     {
-        return array_column(static::cases(), 'name');
+        return array_column(
+            array: self::cases(),
+            column_key: 'name',
+        );
     }
 
     public static function values(): array
     {
-        return array_column(self::cases(), 'value');
+        return array_column(
+            array: self::cases(),
+            column_key: 'value',
+        );
     }
 
-    public function getLabel(): string
-    {
-        return Lang::has($this->getNamespace())
-            ? $this->translated()
-            : $this->defaultLabel();
-    }
-
-    public function toHtml(): string
-    {
-        if (! method_exists(self::class, 'color')) {
-            return $this->getLabel();
-        }
-
-        return sprintf("<span class='badge bg-label-%s'>%s</span>", $this->getColor(), $this->getLabel());
-    }
-
-    public function getComment(): string
+    public static function comment(): string
     {
         $comment = [];
 
         foreach (self::cases() as $item) {
-            $comment[] = implode(': ', [$item->value, $item->label()]);
+            $comment[] = implode(
+                separator: ': ',
+                array: [$item->value, $item->getLabel()],
+            );
         }
 
-        return implode(', ', $comment);
-    }
-
-    public function is(null|self|string $value): bool
-    {
-        if (! $value instanceof self) {
-            $value = self::tryFrom($value);
-        }
-
-        return $this === $value;
-    }
-
-    public function isNot(mixed $value): bool
-    {
-        return ! $this->is($value);
-    }
-
-    public static function tryFromName(string $name): ?static
-    {
-        foreach (self::cases() as $case) {
-            if ($case->name === $name) {
-                return $case;
-            }
-        }
-
-        return null;
-    }
-
-    public static function fromName(string $name): static
-    {
-        return self::tryFromName($name) ?? throw new ValueError($name.' is not a valid case for enum '.self::class);
+        return implode(
+            separator: ' | ',
+            array: $comment,
+        );
     }
 
     public static function options(bool $translated = false): array
     {
         if (! $translated) {
-            return array_column(self::cases(), 'value', 'name');
+            return array_column(
+                array: self::cases(),
+                column_key: 'value',
+                index_key: 'name',
+            );
         }
 
-        return Arr::mapWithKeys(self::cases(), fn (self $case) => [$case->getLabel() => $case->value]);
+        return Arr::mapWithKeys(
+            array: self::cases(),
+            callback: fn (self $case) => [$case->getLabel() => $case->value],
+        );
     }
 
-    private function getNamespace(): string
+    public static function tryFromName(string $name): ?static
     {
-        $class = static::class;
-
-        return "enums.$class.$this->name";
+        try {
+            return constant('static::' . $name);
+        } catch (Error $e) {
+            return null;
+        }
     }
 
-    private function translated(): string
+    public static function fromName(string $name): static
     {
-        return Lang::get($this->getNamespace());
+        return self::tryFromName($name) ?? throw new ValueError($name . ' is not a valid case for enum ' . self::class);
     }
 
-    private function defaultLabel(): string
+    public function badgeClass(): string
     {
-        return Str::of($this->name)->replace(['-', '_'], ' ')->apa();
+        return 'badge badge-';
+    }
+
+    public function getLabel(): string
+    {
+        $string = Str::of($this->name);
+
+        if ($string->contains('_')) {
+            $string = $string->lower();
+        }
+
+        return $string->snake()->replace('_', ' ')->apa()->toString();
+    }
+
+    public function toHtml(): string
+    {
+        if (! method_exists(self::class, method: 'getColor')) {
+            return $this->getLabel();
+        }
+
+        return sprintf(
+            "<span class='%s'>%s</span>",
+            $this->badgeClass() . $this->getColor(),
+            $this->getLabel(),
+        );
+    }
+
+    public function is(null|string|int|UnitEnum $value): bool
+    {
+        if (! $value instanceof UnitEnum && $value !== null) {
+            $value = self::tryFrom((int) $value);
+        }
+
+        return $this === $value;
+    }
+
+    public function isNot(null|string|int|UnitEnum $value): bool
+    {
+        return ! $this->is($value);
     }
 }
